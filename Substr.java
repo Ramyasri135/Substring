471a570743264e28bf81f32f971763f5c76fb1a0
Ramyasri135 <kurvaramyasri4242@gmail.com>
2025-01-25 14:44:42 +0530

    Add files via upload

---
+++ Substr.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Substr {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter a string: ");
+        String a=sc.nextLine();
+        System.out.println(a.substring(0,6));
+    }
+}
